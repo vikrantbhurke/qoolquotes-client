c8e1998b11dfb0a8a2badbf1465b893b3ebb5852
Vikrant Bhurke <bhurkevi@gmail.com>
2024-11-15 02:53:55 +0530

    minor edit

--- src/global/routes/auth.route.tsx
+++
@@ -1,8 +0,0 @@
-import { RootState } from "@/global/states/store";
-import { useSelector } from "react-redux";
-
-export const AuthRoute = ({ clearance, children }: any) => {
-  const { auth } = useSelector((state: RootState) => state.auth);
-
-  return clearance.includes(auth.role) ? children : <></>;
-};
