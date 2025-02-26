9d697ca57ab7361a4757b4f9b1edb301889c06bb
Vikrant Bhurke <bhurkevi@gmail.com>
2025-02-26 12:42:06 +0530

    conditionally render ads based on auth.role for 1 ad

--- src/global/components/layouts/navbar-desktop.layout.tsx
+++ src/global/components/layouts/navbar-desktop.layout.tsx
@@ -1,6 +1,8 @@
 import Banner300x250 from "@/global/ads/Banner300x250";
 import Banner320x50 from "@/global/ads/Banner320x50";
+import { ComponentOneOrTwoRoute } from "@/global/routes";
 import { RootState } from "@/global/states/store";
+import { Clearance } from "@/user/enums";
 import { Center, Space, Stack } from "@mantine/core";
 import { useSelector } from "react-redux";
 
@@ -10,7 +12,16 @@ export const NavbarDesktopLayout = () => {
   return (
     <Stack h="100%" gap="md" justify="space-between">
       <Space />
-      <Center p="md">{isMobile ? <Banner320x50 /> : <Banner300x250 />}</Center>
+
+      <ComponentOneOrTwoRoute
+        clearance={Clearance.LevelThree}
+        compOne={<></>}
+        compTwo={
+          <Center p="md">
+            {isMobile ? <Banner320x50 /> : <Banner300x250 />}
+          </Center>
+        }
+      />
     </Stack>
   );
 };
